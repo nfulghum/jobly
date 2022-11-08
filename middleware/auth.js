292919c53e124599dc893e443c6899288078ff8d
Nick Fulghum <nickfulghum92@gmail.com>
2022-11-08 15:12:08 -0600

    Admin authorization added

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,33 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError("You must be an admin to do that");
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.isAdmin || user.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin
 };
