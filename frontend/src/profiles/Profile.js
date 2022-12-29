7d3ecacac55a78a93e827b7b0c21543e1b9a962e
Nick Fulghum <nickfulghum92@gmail.com>
2022-12-29 14:42:33 -0600

    added profile route

---
+++ frontend/src/profiles/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="Homepage">
+            <div>
+                <h1>Edit Profile</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -6,6 +6,7 @@ import CompanyInfo from '../companies/CompanyInfo';
 import JobList from '../jobs/JobList';
 import Login from '../auth/Login';
 import Signup from '../auth/Signup';
+import Profile from '../profiles/Profile';
 
 
 
@@ -20,6 +21,7 @@ const JoblyRoutes = ({ login, signup }) => {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/login" element={<Login />} login={login} />
                 <Route path="/signup" element={<Signup />} signup={signup} />
+                <Route path="/profile" element={<Profile />} />
                 <Route path="/jobs" element={<JobList />} />
                 <Route path="/companies/:handle" element={<CompanyInfo />} />
                 <Route path="/companies" element={<CompanyList />} />
